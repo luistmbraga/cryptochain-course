a4d501080e58bdc01f6328bddecb792418820a6b
Luís Braga <luistmbraga@gmail.com>
2025-09-09 10:43:45 +0100

    Add files via upload

---
+++ util/crypto-hash.test.js
@@ -0,0 +1,21 @@
+const cryptoHash = require('./crypto-hash');
+
+describe('cryptoHash()', () => {
+    it('generates a SHA-256 hashed output', () => {
+        expect(cryptoHash('hello'))
+        .toEqual('5aa762ae383fbb727af3c7a36d4940a5b8c40a989452d2304fc958ff3f354e7a')
+    });
+
+    it('products the same hash with the input in any order', () => {
+        expect(cryptoHash('one', 'two', 'three'))
+        .toEqual(cryptoHash('one', 'three', 'two', ))
+    });
+
+    it('it produces a unique hash when the properties have changed on an input', () => {
+        const foo = {};
+        const originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
+    });
+});
